d30ea4941da7761f1090aa3ce7ab684e712d53b0
league <league@imac-56.attlocal.net>
2019-05-22 19:55:33 -0700

    CheatSheet, RobotRace IP

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -13,6 +13,7 @@ Robot[] robots = new Robot[5];
 		robots[i] = new Robot();
 	}
 		//4. make each robot start at the bottom of the screen, side by side, facing up
+	
 
 	//5. use another for loop to iterate through the array and make each robot move 
 	//   a random amount less than 50.
